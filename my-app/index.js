7d8d5dfc3732ae4459da42a9fcd55ee0a38b97d4
seon_ <108112575+seon03@users.noreply.github.com>
2024-03-19 19:24:12 +0900

    Add files via upload

---
+++ my-app/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+//import { createRoot } from "react-dom/client";
+import App from './App'; //eslint-disable-line no-unused-vars
+import reportWebVitals from './reportWebVitals';
+
+import Library from './chapter_03/Library';//eslint-disable-line no-unused-vars
+import Clock from './chapter_04/Clock';//eslint-disable-line no-unused-vars
+import CommentList from './chapter_05/CommentList';
+//import NotificationList from './chapter_06/NotificationList';//eslint-disable-line no-unused-vars
+import Accommodate from './chapter_07/Accommodate';
+import ConfirmButton from './chapter_08/ConfirmButton';
+import LandingPage from './chapter_09/LandingPage';
+import AttendanceBook from './chapter_10/AttendanceBook';
+import SignUp from './chapter_11/SignUp';
+import Calculator from './chapter_12/Calculator';
+import ProfileCard from './chapter_13/ProfileCard';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ProfileCard />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
